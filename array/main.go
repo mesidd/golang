0b4faaf9b1f2b3445f5eba104730da329046e4ac
Siddhartha Sharma <siddhartha1234sharma@gmail.com>
2025-12-02 23:18:15 +0530

    array

---
+++ array/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main(){
+	// var item[4]string
+
+	// item[0] ="Apple"
+	// item[2] ="banana"
+
+	// fmt.Println("Items:",item)
+
+	// var num = [5]int {1,2,3,4,5}
+	// fmt.Println("Numbers:",num)
+	// fmt.Println("Length of array:", len(num))
+
+	// var numbers = [4]int{1,2}
+	// fmt.Println(numbers)
+	// fmt.Println(numbers[3])
+
+	var price[5] string
+
+	price[1] = "Ram"
+	price[3] = "Shyam"
+
+	// fmt.Println("Size: %q", price)
+	fmt.Printf("Price Array: %q\n", price)
+}
+
